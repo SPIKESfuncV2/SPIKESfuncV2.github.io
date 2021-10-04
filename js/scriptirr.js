2274e11ae74edf3ad0bf07c00c65a7f33634efd5
22716248 <48638351+22716248@users.noreply.github.com>
2021-10-04 12:12:17 +0800

    Revert "Fix Irreversible Agonist page"
    This reverts commit 313db11e2183d4120960f30eb8062656e315f4b2.

--- js/scriptirr.js
+++ js/scriptirr.js
@@ -295,13 +295,8 @@ function calcLinesIrr(affinity, efficacy, recepDensity, efficiency, agoaffinity,
         agoconc = 10 ** agoconcentration;
         for (i = -12; i < -2; i = i + STEP) {
             data[0].push(i);
-            // Bit of both from client feedback? 
-            // new formula implys removing  / affin from under 10^i 
-            // but this does not produce favorable graphs
-            // this will be replicated in schild page.
-            data[1].push((((10 ** i) / affin) * efcay * recep * efcey * 100) / (((10 ** i) / affin) * (efcay * recep * efcey - efcay + 1 + (agoconc / agoaffin)) + 1 + (agoconc / agoaffin)))
+            data[1].push((((10 ** i)) * efcay * recep * efcey * 100) / (((10 ** i)) * (efcay * recep * efcey + 1 - efcay + (agoconc / agoaffin)) + affin));
         }
-
     }
 
     return data;
